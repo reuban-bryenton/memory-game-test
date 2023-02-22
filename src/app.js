907b1b94fdad0b2c82545dac95a09675e9ea00ed
cbrunie <clement.brunie@picsellia.com>
2023-02-22 10:21:55 +0100

    Add page rendering

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
+const path = require("path")
+
+const makePath = (dir, filename) => {
+    return path.resolve(__dirname, "..", "public", dir, filename)
+}
+
+const todos = ["Get milk", "Get bread", "Learn javascript"]
+
+app.use(bodyParser.json())
+app.set("view engine", "ejs")
+app.use(express.static(path.resolve(__dirname)))
 
 app.get("/", (req, res) => {
-    //TODO: show todo list user
+    res.render(path.resolve(__dirname, "index.ejs"))
 })
 
 app.get("/todo", (req, res) => {
